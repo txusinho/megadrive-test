1cb8854dbf65cda5a3860022a7c3bf0448b9d66f
Txus Ordorika <txusinho@gmail.com>
2017-01-17 00:22:38 +0100

    Complete handling w/o refactor

--- main.c
+++ main.c
@@ -1,18 +1,63 @@
 #include <genesis.h>
 
-int main()
-{
-    VDP_drawText("Hello Genny World!", 10, 13);
+void myJoyHandler(u16 joy, u16 changed, u16 state) {
+    if (joy == JOY_1) {
+        if (state & BUTTON_START) {
+            VDP_drawText("player 1 pressed START button ", 5, 13);
+        } else if (changed & BUTTON_START) {
+            VDP_drawText("player 1 released START button", 5, 13);
+        }
 
-    while(1)
-    {
-        //read input
-        //move sprite
-        //update score
-        //draw current screen (logo, start screen, settings, game, gameover, credits...)
+        if (state & BUTTON_UP) {
+            VDP_drawText("player 1 pressed UP button ", 5, 13);
+        } else if (changed & BUTTON_UP) {
+            VDP_drawText("player 1 released UP button", 5, 13);
+        }
 
-        //wait for screen refresh
+        if (state & BUTTON_DOWN) {
+            VDP_drawText("player 1 pressed DOWN button ", 5, 13);
+        } else if (changed & BUTTON_DOWN) {
+            VDP_drawText("player 1 released DOWN button", 5, 13);
+        }
+
+        if (state & BUTTON_RIGHT) {
+            VDP_drawText("player 1 pressed RIGHT button ", 5, 13);
+        } else if (changed & BUTTON_RIGHT) {
+            VDP_drawText("player 1 released RIGHT button", 5, 13);
+        }
+
+        if (state & BUTTON_LEFT) {
+            VDP_drawText("player 1 pressed LEFT button ", 5, 13);
+        } else if (changed & BUTTON_LEFT) {
+            VDP_drawText("player 1 released LEFT button", 5, 13);
+        }
+
+        if (state & BUTTON_A) {
+            VDP_drawText("player 1 pressed A button ", 5, 13);
+        } else if (changed & BUTTON_A) {
+            VDP_drawText("player 1 released A button", 5, 13);
+        }
+
+        if (state & BUTTON_B) {
+            VDP_drawText("player 1 pressed B button ", 5, 13);
+        } else if (changed & BUTTON_B) {
+            VDP_drawText("player 1 released B button", 5, 13);
+        }
+
+        if (state & BUTTON_C) {
+            VDP_drawText("player 1 pressed C button ", 5, 13);
+        } else if (changed & BUTTON_C) {
+            VDP_drawText("player 1 released C button", 5, 13);
+        }
+    }
+}
+
+int main() {
+    JOY_init();
+    JOY_setEventHandler(&myJoyHandler);
+
+    while (1) {
         VDP_waitVSync();
     }
-    return (0);
+    return 0;
 }
